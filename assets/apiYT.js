37c1be20f0f6b7c9eab4d84459e08afd99e0997e
Mark Selyuzhitskiy <mark.sel9@gmail.com>
2021-10-01 11:06:07 -0400

    Added local storage to save search dates in a list format,when page is refreshed past search dates appear

--- assets/apiYT.js
+++ assets/apiYT.js
@@ -1,4 +1,4 @@
-
+searchHistory = [];
 var searchParamArr = document.location.search.split('=');
 //console.log(searchParamArr);
 var dateValue = searchParamArr[1];
@@ -9,8 +9,24 @@ var todayButton = document.querySelector("#todayBtn")
 //Create element for button on second html page.
 var searchButton = document.querySelector("#searchBtn")
 
+//Get searches out of local storage and append them to the document in a button format
+function getSearches() {
+  for(var i = 1; i < searchHistory.length; i++) {
+    var dateSearched = searchHistory[i];
+
+    var btnEl = document.createElement('li');
+    btnEl.textContent = dateSearched;
+
+    var searchList = document.getElementById('recent-searches');
+    searchList.appendChild(btnEl);
+  }
+}
+
 //API call once user hits search button on homepage.
 function getVideosSearch() {
+  var dateSearch = dateValue;
+
+  searchHistory.push(dateSearch);
   //API key
   const YOUTUBE_API_KEY = "AIzaSyAYAu3YiE2oiiiSFNWemBMC_Kw6uil9pU8";
   //var searchDate = document.getElementById("dateSubmit").value;
@@ -41,9 +57,10 @@ function getVideosSearch() {
 function getVideosSearch2() {
 
   const YOUTUBE_API_KEY = "AIzaSyAYAu3YiE2oiiiSFNWemBMC_Kw6uil9pU8";
-  var searchDate2 = document.getElementById("dateSubmit").value;
-  //console.log(searchDate)
-
+  var searchDate2 = document.getElementById("datepicker").value;
+  console.log(searchDate2);
+  searchHistory.push(searchDate2);
+  console.log(searchHistory);
 
   const url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=" + searchDate2 + "&regionCode=US&safeSearch=moderate&topicId=/m/04rlf&videoSyndicated=true&videoEmbeddable=true&type=video&order=viewCount&key=" + YOUTUBE_API_KEY;
 
@@ -53,6 +70,7 @@ function getVideosSearch2() {
     })
     .then(function (data) {
       console.log(data);
+      localStorage.setItem('searches', JSON.stringify(searchHistory));
 
 
       for (var i = 0; i < 25; i++) {
@@ -108,3 +126,14 @@ $(function () {
 //On click of the search button on second page, this function will fire to load API Youtube fetch.
 searchButton.addEventListener("click", getVideosSearch2);
 
+//local storage
+function inIt() {
+  var storedSearches = JSON.parse(localStorage.getItem('searches'));
+
+  if(storedSearches){
+    searchHistory = storedSearches;
+  }
+  getSearches();
+}
+
+inIt();
